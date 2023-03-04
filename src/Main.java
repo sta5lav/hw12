6000c8682df601f4b600c15798299dada1dc58c6
Stanislav <stdu92@mail.ru>
2023-03-04 22:11:02 +0300

    Объекты и классы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Autor levTolstoy = new Autor("Лев", "Толстой");
+        Book voynaIMir = new Book("Война и мир", levTolstoy, 1863);
+        voynaIMir.printInfo();
+        voynaIMir.setDatePublication(1869);
+        voynaIMir.printInfo();
+        Autor alexandrPushkin = new Autor("Александр", "Пушкин");
+        Book eugeniyOnegin = new Book("Евгений Онегин", alexandrPushkin, 1831);
+        eugeniyOnegin.printInfo();
+        eugeniyOnegin.setDatePublication(1832);
+        eugeniyOnegin.printInfo();
     }
 }
