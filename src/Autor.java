dc46f38a7fca187fd666e33864d568033c47da54
Stanislav <stdu92@mail.ru>
2023-03-07 14:38:12 +0300

    Объекты и классы

--- src/Autor.java
+++ src/Autor.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Autor {
     private final String firstName;
     private final String lastName;
@@ -13,6 +15,19 @@ public class Autor {
 
     public String getLastName() {
         return lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Autor autor = (Autor) o;
+        return Objects.equals(firstName, autor.firstName) && Objects.equals(lastName, autor.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
     }
 }
 
